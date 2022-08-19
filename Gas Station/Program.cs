33e159afd5ca65fb386941da55643a88d9e3f20a
Mazstar04 <mazeedahhamzat@gmail.com>
2022-08-19 11:44:53 +0100

    init

---
+++ Gas Station/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] gas = { 2, 3, 4 };
+int[] cost = { 3, 4, 3 };
+Console.WriteLine(StationIndex(gas, cost));
+static int StationIndex(int[] gas, int[] cost)
+{
+    int index = -1;
+    for (int i = 0; i < gas.Length; i++)
+    {
+        int currentGas = 0;
+        for (int j = i; j < i + gas.Length; j++)
+        {
+            if (j < (i + gas.Length - 1))
+            {
+                if (j < gas.Length)
+                {
+                    currentGas += gas[j];
+                    currentGas -= cost[j];
+                }
+                else
+                {
+                    currentGas += gas[j%gas.Length];
+                    currentGas -= cost[j%gas.Length];
+                }
+                if(currentGas < 0) break;
+            }
+            else
+            {
+                index = i;
+                break;
+            }
+        }
+    }
+
+    return index;
+}
---
+++ Player/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+List<int> Numbers = new List<int> { 1, 5, 233, 7 };
+Console.WriteLine(Player(Numbers));
+
+static bool Player(List<int> Numbers)
+{
+    int playerOne = 0, playerTwo = 0;
+    int Count = Numbers.Count();
+    int i = 0;
+    while (Numbers.Count() > 0)
+    {
+        if (Numbers.Count() == 1)
+        {
+            if (i % 2 == 0) playerOne += Numbers[0];
+            else playerTwo += Numbers[0];
+            Numbers.RemoveAt(0);
+        }
+        else
+        {
+            if (Numbers[0] > Numbers[Numbers.Count() - 1])
+            {
+                if (Numbers[0] > Numbers[1] || Numbers[Numbers.Count - 2] > Numbers[0])
+                {
+                    if (i % 2 == 0) playerOne += Numbers[0];
+                    else playerTwo += Numbers[0];
+                    Numbers.RemoveAt(0);
+                }
+                else
+                {
+                    if (i % 2 == 0) playerOne += Numbers[Numbers.Count - 1];
+                    else playerTwo += Numbers[Numbers.Count - 1];
+                    Numbers.RemoveAt(Numbers.Count - 1);
+                }
+            }
+            else
+            {
+                if (Numbers[Numbers.Count - 1] > Numbers[Numbers.Count - 2] || Numbers[1] > Numbers[Numbers.Count - 1])
+                {
+                    if (i % 2 == 0) playerOne += Numbers[Numbers.Count - 1];
+                    else playerTwo += Numbers[Numbers.Count - 1];
+                    Numbers.RemoveAt(Numbers.Count - 1);
+                }
+                else
+                {
+                    if (i % 2 == 0) playerOne += Numbers[0];
+                    else playerTwo += Numbers[0];
+                    Numbers.RemoveAt(0);
+                }
+            }
+        }
+        i++;
+    }
+    return playerOne > playerTwo || playerOne == playerTwo;
+}
